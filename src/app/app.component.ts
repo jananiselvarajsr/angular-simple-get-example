c5c5ed2010aa1bc381dddc9154f67709aa4ad031
jananiselvarajsr <jananiselvarajsr@gmail.com>
2020-12-03 20:47:12 +0530

    File format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,29 @@
-import { Component, VERSION } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, VERSION } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Janani ' ;
+export class AppComponent {
+  name = "Janani ";
 
-   userId;
-   id;
-   title;
-   completed;
+  userId;
+  id;
+  title;
+  completed;
 
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-    sendGetRequest(userId) {
-        this.http.get<any>('https://jsonplaceholder.typicode.com/todos/1').subscribe(data => {
-            this.userId = data.userId;
-            this.id=data.id;
-            this.completed=data.completed;
-            this.title=data.title;
-        })        
-    }
+  sendGetRequest(userId) {
+    this.http
+      .get<any>("https://jsonplaceholder.typicode.com/todos/1")
+      .subscribe(data => {
+        this.userId = data.userId;
+        this.id = data.id;
+        this.completed = data.completed;
+        this.title = data.title;
+      });
+  }
 }
