ad6f4c4a236224c9fece26074d4c969213275ead
jananiselvarajsr <jananiselvarajsr@gmail.com>
2020-12-04 19:08:06 +0530

    working basic retrieve with service class

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,17 +1,20 @@
 import { Component, VERSION } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from 'rxjs';
 import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root"
 })
 export class AppService {
-  protected url: string = '"https://jsonplaceholder.typicode.com/todos/1"';
+  protected url: string = 'https://jsonplaceholder.typicode.com/todos/1';
 
   constructor(private http: HttpClient) {}
 
-  getAll(): any[] {
-    return this.http.get(this.url);
+  getAll(): Observable<any>  {
+    console.log("here")
+    const data=this.http.get(this.url);
+    return data;
   }
 }
