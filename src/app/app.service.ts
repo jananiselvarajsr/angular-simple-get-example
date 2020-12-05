c665934f87a884620633e56f08d62dac61b9081f
jananiselvarajsr <jananiselvarajsr@gmail.com>
2020-12-05 19:11:20 +0530

    add header

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,20 +1,22 @@
 import { Component, VERSION } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from 'rxjs';
+import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
+import { HttpHeaders } from "@angular/common/http";
 
 @Injectable({
   providedIn: "root"
 })
 export class AppService {
-  protected url: string = 'https://jsonplaceholder.typicode.com/todos/1';
+  protected url: string = "https://jsonplaceholder.typicode.com/todos/1";
 
   constructor(private http: HttpClient) {}
 
-  getAll(): Observable<any>  {
-    console.log("here")
-    const data=this.http.get(this.url);
+  getAll(): Observable<any> {
+    const headers = new HttpHeaders().set("content-type", "application/json");
+    console.log("here");
+    const data = this.http.get(this.url, { headers: headers });
     return data;
   }
 }
