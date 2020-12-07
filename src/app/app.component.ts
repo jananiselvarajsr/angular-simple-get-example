7810dd1bfb09aa7228c89d9bc22c54e7ebb02268
jananiselvarajsr <jananiselvarajsr@gmail.com>
2020-12-07 15:07:31 +0530

    log for reservation response

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,8 @@ export class AppComponent {
   constructor(private appService: AppService) {}
 
   sendGetRequest(userId) {
-    this.appService.getAll().subscribe(data => {
+    this.appService.getAll().subscribe((data: any) => {
+      console.log(data);
       this.userId = data.userId;
       this.id = data.id;
       this.completed = data.completed;
